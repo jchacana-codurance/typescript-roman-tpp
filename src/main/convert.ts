1fdbc26186dd96910b1b9314d364af44481c819b
Javier Chacana <javier.chacana@codurance.com>
2021-05-17 12:06:39 +0200

    REFACTOR moving 1 logic to symbol function

--- src/main/convert.ts
+++ src/main/convert.ts
@@ -12,7 +12,7 @@ export function convert(number: number): string {
     }
     if(number >= 5) return symbol.symbol + convert(number - symbol.value)
     if(number >= 4) return symbol.symbol + convert(number - symbol.value)
-    if(number >= 1) return symbols[1] + convert(number - 1)
+    if(number >= 1) return symbol.symbol + convert(number - symbol.value)
     return symbols[number]
 }
 
@@ -20,5 +20,6 @@ function getSymbolAndValue(number: number): {symbol: string, value: number} {
     if(number >= 9) return {symbol: "IX", value: 9}
     if(number >= 5) return {symbol: "V", value: 5}
     if(number >= 4) return {symbol: "IV", value: 4}
+    if(number >= 1) return {symbol: "I", value: 1}
     return {symbol: "", value: 0}
 }
