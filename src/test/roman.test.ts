7ff0721b62849634e4523ff7a5eefa397c01ed5d
Javier Chacana <javier.chacana@codurance.com>
2021-05-17 11:14:39 +0200

    RED failing test for 1

--- src/test/roman.test.ts
+++ src/test/roman.test.ts
@@ -5,6 +5,10 @@ describe('Roman Numeral converter', () => {
         const roman = convert(0)
         expect(roman).toBe('')
     })
+    it("should return 'I' for 1 ", () => {
+        const roman = convert(1)
+        expect(roman).toBe('I')
+    })
 })
 
 
