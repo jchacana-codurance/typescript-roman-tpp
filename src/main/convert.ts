d2f43c558d07306d753f28c76eac9569c1bef2c2
Javier Chacana <javier.chacana@codurance.com>
2021-05-17 11:43:57 +0200

    REFACTOR use of container to provide value for 5

--- src/main/convert.ts
+++ src/main/convert.ts
@@ -1,10 +1,11 @@
 const symbols: Record<number, string> = {
     0: '',
     1: 'I',
-    4: 'IV'
+    4: 'IV',
+    5: 'V'
 }
 export function convert(number: number): string {
-    if(number >= 5) return 'V'
+    if(number >= 5) return symbols[5]
     if(number >= 4) return symbols[4]
     if(number >= 1) {
         return symbols[1] + convert(number - 1)
