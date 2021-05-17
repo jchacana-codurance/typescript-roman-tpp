5e1ea73c4518035b6d266abc2c495e2d66e4a80a
Javier Chacana <javier.chacana@codurance.com>
2021-05-17 11:35:59 +0200

    RED apply 9th transformation

--- src/main/convert.ts
+++ src/main/convert.ts
@@ -1,7 +1,7 @@
 export function convert(number: number): string {
     let roman = ['', 'I', 'II', 'III', 'IV'];
     if(number >= 1) {
-        return roman[number]
+        return roman[1] + convert(number - 1)
     }
     return roman[number]
 }
