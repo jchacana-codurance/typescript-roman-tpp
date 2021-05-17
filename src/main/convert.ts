00f0a232d9f2a2cc9420dc803b9810d6702c50e3
Javier Chacana <javier.chacana@codurance.com>
2021-05-17 11:39:30 +0200

    REFACTOR apply 8th transformation

--- src/main/convert.ts
+++ src/main/convert.ts
@@ -1,8 +1,14 @@
+const symbols: Record<number, string> = {
+    0: '',
+    1: 'I',
+    2: 'II',
+    3: 'III',
+    4: 'IV'
+}
 export function convert(number: number): string {
-    let roman = ['', 'I', 'II', 'III', 'IV'];
-    if(number >= 4) return roman[4]
+    if(number >= 4) return symbols[4]
     if(number >= 1) {
-        return roman[1] + convert(number - 1)
+        return symbols[1] + convert(number - 1)
     }
-    return roman[number]
+    return symbols[number]
 }
