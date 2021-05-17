0d96ead7c5528510f0c869d7d7ca46c04867605a
Javier Chacana <javier.chacana@codurance.com>
2021-05-17 12:01:07 +0200

    REFACTOR extracting function to determine symbol and value

--- src/main/convert.ts
+++ src/main/convert.ts
@@ -6,11 +6,17 @@ const symbols: Record<number, string> = {
     9: 'IX'
 }
 export function convert(number: number): string {
-    if(number >= 9) return symbols[9]
+    let symbol = getSymbolAndValue(number);
+    if(number >= 9) {
+        return symbol.symbol + convert(number - 9)
+    }
     if(number >= 5) return symbols[5] + convert(number - 5)
     if(number >= 4) return symbols[4]
-    if(number >= 1) {
-        return symbols[1] + convert(number - 1)
-    }
+    if(number >= 1) return symbols[1] + convert(number - 1)
     return symbols[number]
+}
+
+function getSymbolAndValue(number: number): {symbol: string, value: number} {
+    if(number >= 9) return {symbol: "IX", value: 9}
+    return {symbol: "", value: 0}
 }
