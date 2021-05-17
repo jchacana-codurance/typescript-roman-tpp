0fe318f1200e243c7085dd0f7ed74bb69968f8b1
Javier Chacana <javier.chacana@codurance.com>
2021-05-17 11:37:34 +0200

    GREEN apply 6th transformation

--- src/main/convert.ts
+++ src/main/convert.ts
@@ -1,5 +1,6 @@
 export function convert(number: number): string {
     let roman = ['', 'I', 'II', 'III', 'IV'];
+    if(number >= 4) return roman[4]
     if(number >= 1) {
         return roman[1] + convert(number - 1)
     }
