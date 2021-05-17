cd9d157172b7b424fc147b161e85d36119e63fb7
Javier Chacana <javier.chacana@codurance.com>
2021-05-17 11:34:23 +0200

    REFACTOR apply 6th transformation

--- src/main/convert.ts
+++ src/main/convert.ts
@@ -1,4 +1,7 @@
 export function convert(number: number): string {
     let roman = ['', 'I', 'II', 'III', 'IV'];
+    if(number >= 1) {
+        return roman[number]
+    }
     return roman[number]
 }
