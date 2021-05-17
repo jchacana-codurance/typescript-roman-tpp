28b3b1b972aca33e6c8e72ceb889344e2a268fb3
Javier Chacana <javier.chacana@codurance.com>
2021-05-17 11:28:10 +0200

    REFACTOR removed duplicity with parameterized tests

--- src/test/roman.test.ts
+++ src/test/roman.test.ts
@@ -1,21 +1,14 @@
 import {convert} from "../main/convert";
 
 describe('Roman Numeral converter', () => {
-    it("should return '' for 0 ", () => {
-        const roman = convert(0)
-        expect(roman).toBe('')
-    })
-    it("should return 'I' for 1 ", () => {
-        const roman = convert(1)
-        expect(roman).toBe('I')
-    })
-    it("should return 'II' for 2 ", () => {
-        const roman = convert(2)
-        expect(roman).toBe('II')
-    })
-    it("should return 'III' for 3 ", () => {
-        const roman = convert(3)
-        expect(roman).toBe('III')
+    it.each([
+        ["", 0],
+        ["I", 1],
+        ["II", 2],
+        ["III", 3],
+    ])("should return '%s' for %i", (expected: string, number: number) => {
+        const roman = convert(number)
+        expect(roman).toBe(expected)
     })
 })
 
