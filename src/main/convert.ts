e79fc77385cabfc2240c7cafcb8211cd35d3dca3
Javier Chacana <javier.chacana@codurance.com>
2021-05-17 11:46:28 +0200

    GREEN apply 3th transformation then 9th transformation

--- src/main/convert.ts
+++ src/main/convert.ts
@@ -5,7 +5,7 @@ const symbols: Record<number, string> = {
     5: 'V'
 }
 export function convert(number: number): string {
-    if(number >= 5) return symbols[5]
+    if(number >= 5) return symbols[5] + convert(number - 5)
     if(number >= 4) return symbols[4]
     if(number >= 1) {
         return symbols[1] + convert(number - 1)
