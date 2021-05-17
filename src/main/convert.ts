1c1713aed3771718d72a80a9e4ae44042c114392
Javier Chacana <javier.chacana@codurance.com>
2021-05-17 11:42:45 +0200

    GREEN apply 6th transformation then 2nd transformation

--- src/main/convert.ts
+++ src/main/convert.ts
@@ -4,6 +4,7 @@ const symbols: Record<number, string> = {
     4: 'IV'
 }
 export function convert(number: number): string {
+    if(number >= 5) return 'V'
     if(number >= 4) return symbols[4]
     if(number >= 1) {
         return symbols[1] + convert(number - 1)
